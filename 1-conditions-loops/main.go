d725d5f35e4e42d7542f9ece52987c80aedb7006
Joe Greene <jggreene@gmail.com>
2016-03-05 15:06:30 -0800

    completing 1

---
+++ 1-conditions-loops/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	// fmt.Println("my random number is", ,"and is greater than", )
+	 fmt.Println("I am a student of the Holberton School")
+	// fmt.Println("It's a beautiful weather!")
+	// fmt.Println("Rudy Rigot is a founder")
+	// fmt.Println("Sylvain Kalache is a founder")
+	// fmt.Println("Julien Barbier is a founder")
+
+
+	if randomNumber := rand.Intn(100); randomNumber > 50 {
+		fmt.Print("my random number is ", randomNumber, " and is greater than 50")
+		// else
+    fmt.Println()
+
+
+  if school := "Holberton School"; school == "Holberton School"{
+
+    fmt.Println("I am a student of the ", school)
+
+}
+;}
